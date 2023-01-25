4f237d7c291f4ebc21653366538c9eaed5c618dc
Francis Ricardo Ramirez <francisricardor@gmail.com>
2023-01-24 20:03:45 -0400

    Primera subida.

---
+++ itlathAppSol/itlath.Web/Models/StudentController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using itlath.Web.Models;
+namespace itlathApp.Web.Models
+{
+    public class StudentController : Controller
+    {
+        // GET: StudentController
+        public ActionResult Index()
+        {
+            List<StudentModel> students = new List<StudentModel>()
+            {
+                new StudentModel()
+                {
+                    Id = 1,
+                    EnrollmentDate = System.DateTime.Now,
+                    FirstName = "Francis",
+                    LastName ="Ramirez"
+                },
+                new StudentModel()
+                {
+                    Id = 2,
+                    EnrollmentDate = System.DateTime.Now,
+                    FirstName = "Juan",
+                    LastName ="Gonzales"
+                },
+                new StudentModel()
+                {
+                    Id = 3,
+                    EnrollmentDate = System.DateTime.Now,
+                    FirstName = "Jose",
+                    LastName ="Perez"
+                },
+            };
+
+
+            return View(students);
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ itlathAppSol/itlath.Web/Models/StudentModel.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace itlathApp.Web.Models
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
